509bf53c8cfcde4e5002275e0dbcea39f8d48ab5
omchandrasharma <omchandrasharma2005@gmail.com>
2025-04-18 05:04:22 +0530

    fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Auth/Login.jsx";
 import Signup from "./pages/Auth/Signup.jsx";
-import CartPage from "./pages/cart/Cart.jsx"; // adjust path as needed
-
-import "./App.css";
+import CartPage from "./pages/cart/Cart.jsx";
 import Sell from "./pages/Sell_item/Sell.jsx";
 
+import { saveToken } from "./utils/auth.js";
+import "./App.css";
+
 function App() {
-  
+  useEffect(() => {
+    // Generate and store a dummy JWT token
+    const dummyToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+      "eyJ1c2VySWQiOjEsIm5hbWUiOiJKb2huIERvZSJ9." +
+      "s5Ot_uZxqS85fWj6LgOn3z-eGLHKj0KAhUJkI0PGE-Y";
+    saveToken(dummyToken);
+  }, []);
+
   return (
     <Router>
       <Routes>
