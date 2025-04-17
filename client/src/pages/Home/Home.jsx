a155a9a7b47ea8f3bccf6bc39a74044c6cd81b64
omchandrasharma <omchandrasharma2005@gmail.com>
2025-04-17 21:14:14 +0530

    cleaned up

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,37 +1,15 @@
-import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header.jsx";
 import FilterPanel from "../../components/FilterPanel/FilterPanel.jsx";
-// import ProductCard from "../../components/ProductCard/ProductCard.jsx";
-// import styles from "./Home.module.css";
-// import axios from "axios";
-
+import FloatingSellButton from "../../components/FloatingSellButton/FloatingSellButton.jsx";
 
 const Home = () => {
-  // const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     try {
-  //       // Replace with your backend endpoint
-  //       const res = await axios.get("http://localhost:5000/api/products");
-  //       setProducts(res.data.products);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   fetchProducts();
-  // }, []);
-
+  console.log("home rendered");
   return (
     <div>
       <Header />
       <FilterPanel />
-       {/* <div className={styles.productGrid}>
-        {products.map((p) => (
-          <ProductCard key={p._id} product={p} />
-        ))}
-      </div> */}
-    </div> 
+      <FloatingSellButton />
+    </div>
   );
 };
 
